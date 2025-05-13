7c1bced406f87409fc3a5dcc9eb7b4dc67039453
Arpit Dara <109797400+ArpitDara2003@users.noreply.github.com>
2025-05-13 11:45:36 +0530

    Delete src directory

--- src/main/resources/static/js/script.js
+++
@@ -1,6 +0,0 @@
-
-function navigateToPage(element) {
-    var page = element.getAttribute('data-page');
-    console.log('Navigating to:', page); // This is where you handle navigation
-    window.location.href = page; // Example: navigate to the page
-}
